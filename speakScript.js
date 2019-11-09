fc96b1d69f20108149d7d8306160585f0f6ebdeb
Mostafa <mustafa.husseiny@gmail.com>
2019-11-09 23:08:36 +0100

    Add files via upload

--- speakScript.js
+++ speakScript.js
@@ -36,29 +36,29 @@ function mobileMenuList() {
 const userclick = document.getElementById("clickable");
 userclick.onclick = mobileMenuList;
 
-/*
-window.onload = function() {
-    let i = 0;
-    while (i < 3) {
-        i++;
+
+ function holder() {
+        let o = 0;
+    while (o < 3) {
+        o++;
         const targ = document.getElementsByClassName("screen")[0];
         const myTemplate = document.getElementById("templ").content.cloneNode(true);
         targ.append(myTemplate);
     }
-
+    return;
 }
-*/
+//window.onload = holder;
 
 let n = 0;
 function representLeft() {
     n++;
     let mango = document.getElementsByClassName("screen")[0];
     let point = mango.children[0];
-    if (n > 4) {
+    if (n > mango.children.length - 1) {
         n = 0;
     }
     try {
-    point.style.marginLeft = `-${(n * 200)}px`;
+    point.style.marginLeft = `-${(n * 255)}px`;
     } catch(err) {
         alert(err.message);
     }
@@ -71,11 +71,51 @@ function representRight() {
     if ( n < 0) {
         n = 0;
     }
-    point.style.marginLeft = `-${n * 200}px`;
+    point.style.marginLeft = `-${n * 255}px`;
 }
 
 const userRight = document.getElementsByClassName("item-arrow-right")[0];
 userRight.addEventListener("click", representRight, false);
 
 const userLeft = document.getElementsByClassName("item-arrow-left")[0];
-userLeft.onclick = representLeft;
+userLeft.addEventListener("click", representLeft, false);
+
+
+function remover() {
+    let opera = document.getElementsByClassName("screen")[0].children;
+    for (let i = 0; i < opera.length; i++) {
+        opera[i].style.display = "none";
+    }
+
+}
+
+
+   function render() {
+    holder();
+    let url = "https://raw.githubusercontent.com/freelancer2020/SpeakUp/master/contents/level1.html";
+    fetch(url)
+    .then(response => {
+        return response.text();
+    })
+    .then(card => {
+       let parser = new DOMParser();
+       let contentUser =  parser.parseFromString(card, "text/html");
+       let cards = contentUser.getElementsByClassName("tem");
+       let arr = [];
+       for (let x = 0; x < cards.length; x++) {
+           arr.push(cards[x].content.cloneNode(true));
+       }
+       for (let y = 0; y < arr.length; y++) {
+           document.getElementsByClassName("screen")[0].append(arr[y]);
+       }
+       
+    })
+    
+    
+}
+
+
+window.onload = render;
+
+
+
